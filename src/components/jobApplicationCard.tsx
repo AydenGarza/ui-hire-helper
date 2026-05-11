6cfda6b13b6c39fb68e706f58a484b2eeb3afdbe
Ayden Garza <aydenjgarza@gmail.com>
2026-05-10 22:43:28 -0500

    update window

--- src/components/jobApplicationCard.tsx
+++ src/components/jobApplicationCard.tsx
@@ -10,7 +10,16 @@ const statuses = ["Applied", "Interviewing", "Offer", "Rejected"];
 
 const JobApplication = ({ applicationDTO, onUpdateCallback }: Props) => {
 
+	const updateApplication = () => {
+		
+	}
+	
 	const [updating, setUpdating] = useState(false);
+	const [company, setCompany] = useState('')
+	const [jobTitle, setJobTitle] = useState('')
+	const [dateApplied, setDateApplied] = useState('')
+	const [appStatus, setAppStatus] = useState('')
+	
 	
 	return <div className="border rounded-lg px-2 m-2" onClick={() => setUpdating(true)}>
 		<div className="text-textcolor font-semibold py-2">
@@ -20,7 +29,18 @@ const JobApplication = ({ applicationDTO, onUpdateCallback }: Props) => {
 			<div>Date Applied: {applicationDTO.date_applied}</div>
 			<div>Status: {applicationDTO.application_status}</div>
 			<div>Company: {applicationDTO.company}</div>
-			{updating ? "true": "false"}
+			{updating && (
+				<div className="fixed bg-gray-700 flex items-center justify-center z-50">
+					<div className = "flex p-4 gap-5">
+						<input placeholder="Company" value={company} onChange={e => setCompany(e.target.value)} className="border bg-background text-textcolor flex-1 rounded"/>
+						<input placeholder="Job Title" value={jobTitle} onChange={e => setJobTitle(e.target.value)} className="border bg-background text-textcolor flex-1 rounded"/>
+						<input placeholder="Date YYYY-MM-DD" value={dateApplied} onChange={e => setDateApplied(e.target.value)} className="border bg-background text-textcolor rounded"/>
+						<input placeholder="Status" value={appStatus} onChange={e => setAppStatus(e.target.value)} className="border bg-background text-textcolor rounded" />
+						<button className="border p-1 bg-green-400 rounded" onClick={updateApplication}>Update</button>
+						<button className="border p-1 bg-orange-400 rounded" onClick={(e) => { e.stopPropagation();  setUpdating(false)}}> Finished</button>
+					</div>
+				</div>
+			)}
 		</div>
 	</div>
 }
