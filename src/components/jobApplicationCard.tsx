403b64a3c4f1560df266084998031c0f86387363
Ayden Garza <aydenjgarza@gmail.com>
2026-05-10 21:35:58 -0500

    form input for new applications

--- src/components/jobApplicationCard.tsx
+++ src/components/jobApplicationCard.tsx
@@ -5,11 +5,11 @@ type Props = {
 }
 
 const JobApplication = ({ applicationDTO }: Props) => {
-	return <div className="border rounded-lg px-2 m-2 bg-gray-50">
-		<div className="text-base font-semibold py-2">
+	return <div className="border rounded-lg px-2 m-2">
+		<div className="text-textcolor font-semibold py-2">
 			{applicationDTO.job_title}
 		</div>
-		<div className="flex gap-4 justify-between py-2 text-gray-700">
+		<div className="flex gap-4 justify-between py-2 text-textcolor">
 			<div>Date Applied: {applicationDTO.date_applied}</div>
 			<div>Status: {applicationDTO.application_status}</div>
 			<div>{applicationDTO.company}</div>
--- src/pages/KanbanPage.tsx
+++ src/pages/KanbanPage.tsx
@@ -5,6 +5,11 @@ import JobApplication from "../components/jobApplicationCard";
 
 export default function KanbanPage() {
 
+	const [company, setCompany] = useState('')
+	const [jobTitle, setJobTitle] = useState('')
+	const [dateApplied, setDateApplied] = useState('')
+	const [appStatus, setAppStatus] = useState('')
+	
 	async function getApplications() {
 		const jwt = localStorage.getItem("access_token");
 		const meta = {
@@ -18,12 +23,20 @@ export default function KanbanPage() {
 	}
 	
 	const [applications, setApplications] = useState<JobApplicationDTO[]>([]);
-	useEffect(() => {getApplications()}, [])
+	useEffect(() => { getApplications() }, [])
+	
 	return (
 		<div className="bg-background flex flex-col gap-3">
 			{
 				applications.map(application => ( <JobApplication key={application.id} applicationDTO={application}></JobApplication>))
 			}
+
+			<div className = "flex">
+				<input placeholder="Company..." value={company} onChange={e => setCompany(e.target.value)}/>
+				<input placeholder="Job Title..." value={jobTitle} onChange={e => setJobTitle(e.target.value)}/>
+				<input placeholder="Date Applied... YYYY-MM-DD" value={dateApplied} onChange={e => setDateApplied(e.target.value)}/>
+				<input placeholder="Status..." value={appStatus} onChange={e => setAppStatus(e.target.value)}/>
+			</div>
 		</div>
 	)
 }
